eec3750907bca55944030db2a14a177ba6f1061e
Alex <acorrea21@alumnos.utalca.cl>
2023-09-07 09:57:42 -0300

    test

--- app/src/main/java/cl/acorrea/moveit/MainActivity.kt
+++ app/src/main/java/cl/acorrea/moveit/MainActivity.kt
@@ -3,15 +3,11 @@ package cl.acorrea.moveit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-
-
-
     }
 }
